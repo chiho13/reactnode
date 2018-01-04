43cfd5263b215a445774c212ec77d36951371f54
Anthony <a.chiho13@gmail.com>
2018-01-04 20:31:02 +0000

    first commit

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    "218448359512-5empoq9vrv8a69nh9lmmpg8e12sir5cd.apps.googleusercontent.com",
+  googleClientSecret: "JWDMELnmc2kMQrxiuwK4YwpJ",
+  mongoURI: "mongodb://anthony:password@ds133077.mlab.com:33077/anygood-dev",
+  cookieKey: "usnkfsnfklfs"
+};
