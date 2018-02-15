399bc4894e064cf40c482f7db8b36d1e06bf29ef
Anthony <a.chiho13@gmail.com>
2018-02-15 21:41:03 +0000

    refactor

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import { connect } from 'react-redux';
-import * as actions from '../actions';
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
-import Header from './Header';
+import Header from "./Header";
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
@@ -27,6 +27,6 @@ class App extends Component {
       </div>
     );
   }
-};
+}
 
 export default connect(null, actions)(App);
--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,22 @@
-import 'materialize-css/dist/css/materialize.css';
+import "materialize-css/dist/css/materialize.min.css";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import reduxThunk from 'redux-thunk';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
 
-import App from './components/App';
-import reducers from './reducers';
+import App from "./components/App";
+import reducers from "./reducers";
 
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from "./registerServiceWorker";
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-   document.getElementById('root'));
+  document.getElementById("root")
+);
 registerServiceWorker();
