30fd064ca8cdb41c0fba2c7629b5be29a93eba22
Anthony <a.chiho13@gmail.com>
2018-04-16 21:45:59 +0100

    heroku production

--- index.js
+++ index.js
@@ -26,6 +26,17 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if(process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like main.js file or main.css file
+  app.use(express.static('client/build'));
+  //Express will serve up the index.html if it doesn't recognise the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
